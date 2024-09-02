dafceb1e7d237866d7f2da9922d4ee4d9e4e6b23
Daniel Teel <daniel.teel.2@us.af.mil>
2024-09-01 21:29:01 -0600

    asd

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 #include "utils.h"
 #include "secrets.h"
 #include "net.h"
-#include "DHTesp.h" 
+//#include "DHTesp.h" 
 
 #include <Wire.h>
 #include <SPI.h>
@@ -16,11 +16,8 @@ const char *WiFiPass = SECRET_WIFI_PASS;
 Net NetClient("device1", SECRET_ENCROKEY, "192.168.50.178", 4004);
 
 
-DHTesp dhtSensor1;
+//DHTesp dhtSensor1;
 Adafruit_BME280 bme; // use I2C interface
-Adafruit_Sensor *bme_temp = bme.getTemperatureSensor();
-Adafruit_Sensor *bme_pressure = bme.getPressureSensor();
-Adafruit_Sensor *bme_humidity = bme.getHumiditySensor();
 
 
 void packetReceived(uint8_t* data, uint32_t dataLength){
@@ -51,12 +48,17 @@ void setup(){
     Serial.println(ESP.getPsramSize());
 
     
-	dhtSensor1.setup(17, DHTesp::DHT22);
+	//dhtSensor1.setup(17, DHTesp::DHT22);
     
     Wire.setPins(16, 15);
     if (!bme.begin(0x76)){
         Serial.println(F("Could not find a valid BME280 sensor, check wiring!"));
     }
+    bme.setSampling(Adafruit_BME280::MODE_FORCED,
+                    Adafruit_BME280::SAMPLING_X1, // temperature
+                    Adafruit_BME280::SAMPLING_X1, // pressure
+                    Adafruit_BME280::SAMPLING_X1, // humidity
+                    Adafruit_BME280::FILTER_OFF   );
   
 }
 
@@ -78,21 +80,12 @@ void loop(){
 
     if (NetClient.loop()){
         if (isTimeToExecute(lastPrintTime, 1000)){
-            NetClient.sendString(String("CPU: ")+String(temperatureRead()));
-            NetClient.sendString(String("\n"));
-            NetClient.sendString(String("T1: ")+String(dhtSensor1.getTemperature()));
-            NetClient.sendString(String("H1: ")+String(dhtSensor1.getHumidity()));
-            
-            sensors_event_t temp_event, pressure_event, humidity_event;
-            bme_temp->getEvent(&temp_event);
-            bme_pressure->getEvent(&pressure_event);
-            bme_humidity->getEvent(&humidity_event);
-
-            
-            NetClient.sendString(String("T2: ")+String(temp_event.temperature));
-            NetClient.sendString(String("H2: ")+String(humidity_event.relative_humidity));
-            NetClient.sendString(String("P2: ")+String(pressure_event.pressure));
-            NetClient.sendString(String("\n\n"));
+            // NetClient.sendString(String("CPU: ")+String(temperatureRead()));
+            // NetClient.sendString(String("\n"));
+            // NetClient.sendString(String("T1: ")+String(dhtSensor1.getTemperature()));
+            // NetClient.sendString(String("H1: ")+String(dhtSensor1.getHumidity()));
+            bme.takeForcedMeasurement();
+            NetClient.sendString(String(millis())+","+String(bme.readTemperature())+String(",")+String(bme.readHumidity())+String(",")+String(bme.readPressure())+String(",")+String(bme.readAltitude(SENSORS_PRESSURE_SEALEVELHPA)));
         }
     }
 }
